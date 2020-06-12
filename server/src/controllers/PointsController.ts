1f6b9164ecff9b64bda6c43bcaacdbcf5bfb7c82
Fernando A. M. Leite <fernandoaml@outlook.com>
2020-06-12 13:59:56 -0300

    fix image

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -38,7 +38,8 @@ class PointsController {
             const { name, email, whatsapp, latitude, longitude, city, uf, items } = request.body;
 
             const point = {
-                image: "image-fake",
+                image:
+                    "https://images.unsplash.com/photo-1556767576-5ec41e3239ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=80",
                 name,
                 email,
                 whatsapp,
